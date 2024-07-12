ef936df31c7910d1908faf660bce04a21cfaa06b
llizibends <lizi.bendeliani.1@btu.edu.ge>
2024-07-12 14:06:45 +0400

    Update ლიზი ბენდელიანი.py

--- ლიზი ბენდელიანი.py
+++ ლიზი ბენდელიანი.py
@@ -23,34 +23,3 @@
 # #     line=str('7 / '+str(num)+'='+str(7/num))
 # #     print(line)
 # #     inFile.write(line)
-
-
-# 3. username=('Lizi','Nino','Teo','Nini')
-# looper1=username.__iter__()
-# print(looper1.__next__())
-# print(looper1.__next__())
-# print(looper1.__next__())
-# print(looper1.__next__())
-# print(looper1.__next__())
-
-# 4.პითონში %d და %s არის ფორმატირების კოდები, 
-# რომლებიც გამოიყენება სტრიქონების ფორმატირების ძველ მეთოდებში. 
-# ისინი მოქმედებენ როგორც ჩანაცვლების დამფუძნებლები სტრიქონის ლიტერალში,
-# რომლებიც შეიცვლება ფაქტობრივი მნიშვნელობებით პროგრამის შესრულების დროს.
-
-# 5.city={1:'Kutaisi','კუთხე':'imereti', 'ღირსშანიშნაობა':['ბაგრატი']}
-# del city['კუთხე']
-# print(city)
-
-# 6. try:
-#     for i in range(20):
-#         print(i)
-#         if i == 5:
-#             raise StopIteration("Stopping the iteration at", i)
-# except StopIteration as e:
-#     print("Iteration stopped:", e)
-
-
-# 8. my_list = [("ვაშლი", 3), ("ბანანი", 6), ("ფორთოხალი", 4)]
-# for fruit, quantity in my_list:
-#     print(f"მე მაქვს {quantity} {fruit}")
